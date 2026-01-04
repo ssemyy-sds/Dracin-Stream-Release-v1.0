d92e21db8d2b18e59569612704ac06bfa2aff652
ssemyy-sds <samuelsuryanata90@gmail.com>
2026-01-04 22:41:44 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          orange: '#FF6600',
+          black: '#000000',
+          dark: '#121212',
+          gray: '#1E1E1E'
+        }
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
