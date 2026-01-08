3d969d6ecb233a5689c33b9218b9a2b68bb055e6
ssemyy-sds <samuelsuryanata90@gmail.com>
2026-01-08 17:52:39 +0700

    fix: update Tailwind content config to exclude node_modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./index.tsx",
+    "./App.tsx",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
   ],
   darkMode: 'class',
   theme: {
